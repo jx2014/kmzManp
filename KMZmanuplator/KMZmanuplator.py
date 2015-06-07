1a5c6d54c24b6cea775660b0106bc6a98af9045e
JX_Ultra <jx@jx.com>
2015-06-07 05:24:59 -0700

    working version
    Signed-off-by: JX_Ultra <jx@jx.com>

--- KMZmanuplator/KMZmanuplator.py
+++ KMZmanuplator/KMZmanuplator.py
@@ -1,21 +1,32 @@
 import re
 import os
 import zipfile
+from lxml import etree
+from pykml import parser
+from pykml.factory import KML_ElementMaker as KML
 
 
 class KMZ():
-    def __init__(self, target, kmzfile):
-        #self.kmzDir = os.path.dirname(kmzfile)
+    def __init__(self, target, kmzfile, color='ff0000ff', scale=1.0, visible=1, kmlremove = True):
+        self.mainDir = os.path.dirname(kmzfile)
         #self.kmzFileNameOnly = os.path.basename(kmzfile) #get file name of the kmz file
         self.kmzFile = kmzfile # this is the kmz file with its full path
         self.macFile = target # target is the mac address data file, full path included, which should be the same as kmz file
         self.kmlList = []
+        self.kmlPlacemarkColor = color
+        self.kmlPlacemarkScale = scale
+        self.kmlOtherPlacemarkVisible = visible # by default, turn off all other place mark
+        self.removeKmls = kmlremove
         
-        #load target mac ID to a list
+        
+        # load target mac ID to a list
         self.macList = []
         with open(self.macFile) as fn:
             for mac in fn.readlines():
-                self.macList.append(mac)
+                self.macList.append(mac.rstrip('\n'))
+        # remove duplicated macs
+        self.macList = list(set(self.macList))
+        self.macList = filter(None, self.macList)
     
     # rename file extension KMZ to zip and vise versa
     def RenExt(self, fn, newExt):
@@ -86,16 +97,91 @@ class KMZ():
         for i, filename in enumerate(fileList):
             fileList[i] = '\\'.join([dirname, filename])
         
-        print 'creating ZIP file %s in %s' % (fileNameOnly, dirname)
+        print '...creating ZIP file %s in %s' % (fileNameOnly, dirname)
         
         zipFile = zipfile.ZipFile(newKmzFileName, mode='w')
         try:
             for i, n in enumerate(fileList):
-                print 'adding %s' % n
+                print '   adding %s' % n
                 zipFile.write(n, arcname=os.path.basename(n), compress_type=zipfile.ZIP_DEFLATED)
         finally:
-            print 'Zip finished...'
-            zipFile.close        
+            print '...Zip finished...'
+            zipFile.close
+    
+    # parser kml file from the kmlFile list
+    def KmlParser(self):
+        # test if kml list is empty
+        if len(self.kmlList) == 0:
+            print "kml list is empty, good bye!"
+            return 0       
+        
+        # make a copy of mac list
+        macList = self.macList[:]
+        # count number of mac found in kml
+        n = 1
+        for kml in self.kmlList:            
+            if "style" not in kml: # we don't care about style file
+                kmlPath = '\\'.join([self.mainDir, kml])
+                with open(kmlPath) as f:
+                    doc = parser.parse(f)
+                    doc = doc.getroot()                    
+                    # matching mac from target list with kml list                    
+                    for i in doc.Document.Placemark:
+                        n = n + 1
+                        if i.name in macList:
+                            print "%s found in KML" % i.name
+                            self.KmlPlacemarkColor(KML=i, color=self.kmlPlacemarkColor)
+                            self.KmlPlacemarkScale(KML=i, scale=self.kmlPlacemarkScale)
+                            self.KmlPlacemarkVisiblity(KML=i, visible=1)
+                            # define kml label style properties:                          
+                            newlabel = KML.LabelStyle(KML.color("ffffd500"), 
+                                                      KML.scale(1.0))                            
+                            self.KmlPlacemarkLabel(KML=i, label=newlabel, info=0)
+                            # mac found, style been edited, remove it from list.
+                            macList.remove(i.name)
+                        else:                             
+                            # set visibility of irrelevant placemark                            
+                            self.KmlPlacemarkVisiblity(KML=i, visible=self.kmlOtherPlacemarkVisible, info=0)
+                #save the file
+                print "...saving file... %s" % kmlPath
+                ofile = file(kmlPath, 'w')
+                ofile.write(etree.tostring(etree.ElementTree(doc), pretty_print=True))
+        self.KmlSummary(n, newMacList=macList)             
+    
+    def KmlPlacemarkColor(self, KML, color, info=1):
+        if info==1: print "change color from %s to %s" % (KML.Style.IconStyle.color, color)
+        KML.Style.IconStyle.color = color
+    
+    def KmlPlacemarkScale(self, KML, scale=1.0, info=1):
+        if info==1: print "change icon scale from %s to %s" % (KML.Style.IconStyle.scale, scale)
+        KML.Style.IconStyle.scale = scale
+    
+    def KmlPlacemarkVisiblity(self, KML, visible=1, info=1):
+        if info==1: print "change visibility to %s" % (visible)
+        KML.visibility = visible
+    
+    def KmlPlacemarkLabel(self, KML, label, info=1):
+        KML.Style.append(label)        
+    
+    def KmlSummary(self, numMacs, newMacList=[]):
+        # number of macs that has went through the style change
+        # len(newMacList) may not be zero if target mac are not found in kml file
+        numMacs = len(set(self.macList) - set(newMacList))
+        
+        print "--------      Summary      --------"
+        print "Total MACs found %d" % numMacs
+        print "Changed icon color for %d macs" % numMacs
+        if len(newMacList) > 0:
+            print "Number of Macs not found: %d" % len(newMacList)            
+            for i in newMacList:
+                print i
+        print "--------   End of Summary  --------"
+    
+    # remove files from HDD, i.e. kml files
+    def FilesRemove(self, dirname, fileList = []):
+        for i in fileList:
+            print '...removing... %s from %s' % (i, dirname)
+            os.remove(os.path.join(dirname, i))
     
     # test function
     def TestFunc(self):
@@ -104,11 +190,18 @@ class KMZ():
         print self.kmlList
         print self.macList
         
+        # process the kml file
+        self.KmlParser()
+        
         # generating a new KMZ file name
-        #newKmzFileName = self.NameGen(self.macFile, self.kmzFile)
+        newKmzFileName = self.NameGen(self.macFile, self.kmzFile)
         
         # zip the KML files to this new KMZ file 
-        #self.NewZip(newKmzFileName, zipfiles = self.kmlList)
+        self.NewZip(newKmzFileName, zipfiles = self.kmlList)
+        
+        #remove remaining kml files
+        if self.removeKmls: 
+            self.FilesRemove(dirname=self.mainDir, fileList=self.kmlList)
                         
     
     
--- KMZmanuplator/main.py
+++ KMZmanuplator/main.py
@@ -1,10 +1,39 @@
 from KMZmanuplator import KMZ
 
+def ColorD2H(dec, alpha=255):
+    '''
+        RGBA
+        Red=255 Green=18 Blue=25 alpha=255
+        i.e. ColorD2H(255018025, alpha=255)   
+    '''
+    if len(str(dec)) !=9 or len(str(alpha)) !=3:
+        print "Must use three digits to represent each color and alpha channel"
+        return 0
+    else:
+        red = dec / 1000000
+        green = (dec  - red * 1000000) / 1000
+        blue = (dec - red * 1000000 - green * 1000)        
+    
+    if red > 255 or green > 255 or blue > 255:
+        print "Each color must be less than or equal to 255"
+        return 0
+    else:
+        red = hex(red).split('x')[1].split('L')[0]
+        green = hex(green).split('x')[1].split('L')[0]
+        blue = hex(blue).split('x')[1].split('L')[0]
+        alpha = hex(alpha).split('x')[1].split('L')[0]
+        
+        return red+green+blue+alpha
+    
+    
+
 FPL_weakRF_all = r"C:\Users\jxue\Documents\Projects_LocalDrive\FPL Units\Map\RF_weakRF.txt"
 FPL_weakRF_8 = r"C:\Users\jxue\Documents\Projects_LocalDrive\FPL Units\Map\FPL_weakRF_8.txt"
 KMZ_file = r"C:\Users\jxue\Documents\Projects_LocalDrive\FPL Units\Map\lorenzo-fpl-mia-2015-06-01--23-35-29.meters-bystate.kmz"
 
-newKMZ = KMZ(target = FPL_weakRF_all, kmzfile = KMZ_file)
+hightLight = ColorD2H(255255128) # yellow
+
+newKMZ = KMZ(target=FPL_weakRF_all, color=hightLight, kmzfile=KMZ_file, visible=0)
 
 #newKMZ.renExt(KMZ_file, 'zip')
 
